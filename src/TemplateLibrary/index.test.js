fe182ef28f2fd587bbfabda598160ffbaf2f32d5
irmerk <jolenelanglinais@gmail.com>
2019-04-26 11:39:14 -0400

    feat(*): configure jest setup files - I6
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/TemplateLibrary/index.test.js
+++ src/TemplateLibrary/index.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import Adapter from 'enzyme-adapter-react-16';
 
 import TemplateLibrary from './index';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 describe('<TemplateLibrary />', () => {
   describe('on initialization', () => {
