bd5ef1a652a6a90f75b1b99ad0d3e02c2263d84c
Diana Lease <dianarlease@gmail.com>
2019-05-09 15:12:38 -0400

    test(ErrorLogger): add snapshot and configure jest mock
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/ErrorLogger/index.js
+++ src/ErrorLogger/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'react-table/react-table.css';
 import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 /**
  * @param {object} d
