905975b506141599923e69235f38926b50ed1f1c
Diana Lease <dianarlease@gmail.com>
2019-05-09 14:47:54 -0400

    feat(ErrorLogger): migrate error logger and refactor as functional component
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import TemplateLibrary from './TemplateLibrary';
 import ContractEditor from './ContractEditor';
+import ErrorLogger from './ErrorLogger';
+import TemplateLibrary from './TemplateLibrary';
 import Tile from './Tile';
 import 'semantic-ui-css/semantic.min.css';
 
-export { ContractEditor, TemplateLibrary, Tile };
+export {
+  ContractEditor,
+  ErrorLogger,
+  TemplateLibrary,
+  Tile,
+};
