d41bc9104f9ed7c5e559306b1b68e8b2c0156e7c
Dan Selman <danscode@selman.org>
2019-05-10 13:14:46 +0100

    (chore) merge
    Signed-off-by: Dan Selman <danscode@selman.org>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import ContractEditor from './ContractEditor';
 import ErrorLogger from './ErrorLogger';
 import TemplateLibrary from './TemplateLibrary';
-import ContractEditor from './ContractEditor';
 import ClauseEditor from './ClauseEditor';
 import Tile from './Tile';
 import 'semantic-ui-css/semantic.min.css';
 
 export {
   ContractEditor,
+  ClauseEditor,
   ErrorLogger,
   TemplateLibrary,
   Tile,
