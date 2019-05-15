250fbd0f33c4ba917b1f2dd3d67c2afd84a5ee23
Dan Selman <danscode@selman.org>
2019-05-15 12:47:23 +0100

    (chore) unset lockText for demo
    Signed-off-by: Dan Selman <danscode@selman.org>

--- demo/src/index.js
+++ demo/src/index.js
@@ -29,7 +29,7 @@ function Demo(props) {
   return (
     <div>
       <TemplateLoadingClauseEditor
-        lockText={true}
+        lockText={false}
         markdown={initialMarkdown}
         templateUrl={templateUrl}
         onChange={onChange}
