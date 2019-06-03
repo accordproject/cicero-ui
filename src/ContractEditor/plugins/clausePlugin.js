f66912d5ccd34ff43db3dbb8771f5de388b5e31d
Diana Lease <dianarlease@gmail.com>
2019-06-03 16:33:52 -0400

    chore(markdown-editor): update to latest markdown-editor
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/ContractEditor/plugins/clausePlugin.js
+++ src/ContractEditor/plugins/clausePlugin.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
-import ClauseComponent from '../components/ClauseComponent';
+import ClauseComponent from '../../components/ClauseComponent';
 
 const StyledIcon = styled(Icon)`
   color: #ffffff !important;
