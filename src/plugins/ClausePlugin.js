015a8cfbf7d1e2a38add50bad15795f3cdb77df4
Diana Lease <dianarlease@gmail.com>
2019-07-11 16:25:26 -0400

    chore(ClausePlugin): export ClausePlugin
    Signed-off-by: Diana Lease <dianarlease@gmail.com>

--- src/plugins/ClausePlugin.js
+++ src/plugins/ClausePlugin.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Point } from 'slate';
-import PropTypes from 'prop-types';
 import { Icon } from 'semantic-ui-react';
 import styled from 'styled-components';
 import { Template, Clause } from '@accordproject/cicero-core';
