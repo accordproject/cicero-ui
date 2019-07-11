02e56ce319f1755554ba2c432e073933b3dc4a47
irmerk <jolenelanglinais@gmail.com>
2019-07-11 16:43:15 -0400

    feat(ErrorLogger): modularize actions and styles - I17
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/ErrorLogger/styles.js
@@ -0,0 +1,118 @@
+import styled from 'styled-components';
+import { Icon } from 'semantic-ui-react';
+
+export const ErrorDisplay = styled.div`
+    bottom: 25px;
+    width: 100%;
+    position: fixed;
+    max-height: 300px;
+    background-color: #1E2D53;
+    box-shadow: 0 -2px 20px 0 rgba(20,31,60,0.65);
+`;
+
+export const ErrorsHeader = styled.div`
+    width: 100%;
+    position: fixed;
+    bottom: ${props => (props.errors ? '0' : 'auto')};
+    transition: 1s;
+    height: ${props => (props.errors ? '25px' : '0')};
+    padding: 0.1em 0.1em 0.1em 1em;
+    background-color: ${props => (props.backgroundColor ? '#364C77' : '#1E2D53')};
+    box-shadow: 0 -2px 20px 0 rgba(20,31,60,0.65);
+    border-top: 1px solid #50637F;
+
+    color: ${props => (props.errors ? '#FF4242' : '#19C6C7')};
+    font-family: "IBM Plex Sans";
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: -0.5px;
+    line-height: 20px;
+
+    &:hover {
+        background-color: #364C77;
+        cursor: pointer;
+    }
+`;
+
+export const ErrorSymbol = styled(Icon)`
+    vertical-align: middle;
+`;
+
+export const ErrorBarArrow = styled.div`
+    float: right;
+    margin: 5px 15px;
+
+    border-top: ${props => (props.errorDisplay ? '7px solid #7B9AD1' : '0')};
+    border-right: 4px solid transparent;
+    border-left: 4px solid transparent;
+    border-bottom: ${props => (props.errorDisplay ? '0' : '7px solid #7B9AD1')};
+`;
+
+export const ErrorComponent = styled.div`
+    width: 100%;
+    color: #F0F0F0;
+    border-bottom: 1px solid #50637F;
+    padding: 10px 16px;
+
+    display: grid;
+    grid-row-gap: 20px; 
+    grid-template-areas: "errorArrow errorFile errorType errorMessage"
+                        "errorFull errorFull errorFull errorFull";
+    grid-template-columns: 0.25fr 1fr 1fr 8fr;
+    grid-template-rows: min-content auto;
+`;
+
+export const ErrorFile = styled.a`
+    text-decoration: underline;
+
+    color: #FFFFFF;
+    font-family: "IBM Plex Sans";
+    font-size: 13px;
+    line-height: 13px;
+
+    grid-area: errorFile;
+    align-self: center;
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export const ErrorType = styled.div`
+    grid-area: errorType;
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 13px;
+    line-height: 13px;
+    align-self: center;
+`;
+
+export const ErrorShortMessage = styled.div`
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 13px;
+    line-height: 13px;
+    grid-area: errorMessage;
+    align-self: center;
+`;
+
+export const ErrorFullMessage = styled.div`
+    grid-area: errorFull;
+
+    color: #FFFFFF;
+    font-family: "IBM Plex Sans";
+    font-size: 13px;
+    line-height: 13px;
+
+    // align-self: center;
+`;
+
+export const ArrowDiv = styled.div`
+    grid-area: errorArrow;
+    place-self: center;
+    width: 0; 
+    height: 0; 
+    border-top: ${props => (props.expanded ? '10px solid #50637F' : '4px solid transparent')};
+    border-right: ${props => (props.expanded ? '4px solid transparent' : '0')};
+    border-bottom: ${props => (props.expanded ? '0' : '4px solid transparent')};
+    border-left: ${props => (props.expanded ? '4px solid transparent' : '10px solid #50637F')};
+`;
