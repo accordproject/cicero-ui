ce037da4fdb0e528d118676b71508b4ad00661fc
Jolene Langlinais <36460856+irmerk@users.noreply.github.com>
2019-07-12 13:02:04 -0400

    feat(ErrorLogger): initiate component to handle all errors - I17 (#80)
    chore(package): remove react-table - I17
    feat(ErrorLogger): support multiple collapsing errors - I17
    feat(ErrorLogger): handle error props to determine number - I17
    feat(ErrorLogger): generate all errors dynamically - I17
    feat(ErrorLogger): abstract out functionality for clean render - I1
    chore(package): abstract ramda out to tsv2 - I17
    feat(ErrorLogger): modularize actions and styles - I17
    feat(ErrorLogger): remove file location functionality - I17
    feat(ErrorLogger): add identifiers to error components - I17
    test(ErrorLogger): initiate tests for error rendering - I17
    chore(ErrorLogger): remove comments - I17

---
+++ src/ErrorLogger/actions.js
@@ -0,0 +1,41 @@
+export const gtZero = input => input > 0;
+
+export const typeSwitchCase = (input) => {
+  switch (true) {
+    case ('modelError' in input):
+      return 'Model';
+    case ('parseError' in input):
+      return 'Grammar';
+    default:
+      return 'Unknown';
+  }
+};
+
+export const keySwitchCase = (input) => {
+  switch (true) {
+    case ('modelError' in input):
+      return input.clauseTemplateId;
+    case ('parseError' in input):
+      return input.clauseId;
+    default:
+      return null;
+  }
+};
+
+export const overalltypeSwitchCase = (input) => {
+  switch (true) {
+    case ('modelError' in input):
+      return input.modelError;
+    case ('parseError' in input):
+      return input.parseError;
+    default:
+      return null;
+  }
+};
+
+export const errorsExist = errors => gtZero(errors.length);
+
+export const errorArrayLength = errors => (errorsExist(errors) ? errors.length : 'No');
+
+export const truncateMessage = string => ((string.length > 200)
+  ? `${string.substring(0, 200)}...` : string);
--- src/ErrorLogger/index.js
+++ src/ErrorLogger/index.js
@@ -1,118 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-// import ReactTable from 'react-table';
-import styled from 'styled-components';
-import 'react-table/react-table.css';
 
-const ErrorsBar = styled.div`
-  width: 100vw;
-  height: 100px;
-  padding: 10px;
-  background-color: #1E2D53;
-  box-shadow: 0 -2px 20px 0 rgba(20,31,60,0.65);
-`;
-
-const ErrorsHeader = styled.div`
-  height: 20px;
-  width: 66px;
-  color: ${props => (props.errors ? '#c71a19' : '#19c6c7')};
-  transition: 0.5s;
-  font-family: "IBM Plex Sans";
-  font-size: 16px;
-  font-weight: bold;
-  letter-spacing: -0.5px;
-  line-height: 20px;
-`;
-
-// /**
-//  * @param {object} d
-//  * @param {string} key
-//  */
-// function buildMessage(d, key) {
-//   let result = 'Unknown';
-
-//   if (d.fileLocation) {
-//     result = '';
-//     if (d.fileLocation[key].line) {
-//       result += `Line: ${d.fileLocation[key].line}`;
-//     }
-
-//     if (d.fileLocation[key].column) {
-//       result += ` Col: ${d.fileLocation[key].column}`;
-//     }
-//   }
-
-//   return result;
-// }
-
-// /**
-//  * @param {object} d
-//  */
-// function buildStartLocation(d) {
-//   return buildMessage(d, 'start');
-// }
-
-// /**
-//  * @param {object} d
-//  */
-// function buildEndLocation(d) {
-//   return buildMessage(d, 'end');
-// }
-
-const errorsExist = err => err.length > 0;
-
-const errorsLength = err => err.length || 'No';
-
-
-// const columns = [{
-//   Header: 'Type',
-//   accessor: 'type',
-//   width: 100,
-// },
-// {
-//   Header: 'Name',
-//   accessor: 'name',
-//   width: 200,
-// },
-// {
-//   Header: 'File',
-//   accessor: 'fileName',
-//   width: 100,
-// },
-// {
-//   id: 'startLocation',
-//   Header: 'Start Location',
-//   accessor: d => buildStartLocation(d),
-//   width: 100,
-// },
-// {
-//   id: 'endLocation',
-//   Header: 'End Location',
-//   accessor: d => buildEndLocation(d),
-//   width: 100,
-// },
-// {
-//   Header: 'Message',
-//   accessor: 'shortMessage',
-//   width: 600,
-// },
-// ];
+import * as actions from './actions';
+import * as styles from './styles';
 
 const ErrorLogger = (props) => {
-  console.log('props: ', props.errors);
+  const { errors } = props;
+
+  const [errorsVisible, setErrorsVisible] = useState(false);
+  const [specErrorVisible, setspecErrorVisible] = useState(false);
+
+  const handleClickErrorsBar = () => {
+    if (actions.gtZero(errors.length)) { setErrorsVisible(!errorsVisible); }
+  };
+
+  const handleClickSpecError = () => {
+    setspecErrorVisible(!specErrorVisible);
+  };
+
+  const headerProps = {
+    backgroundColor: errorsVisible,
+    errors: actions.errorsExist(errors),
+    onClick: handleClickErrorsBar
+  };
+
+  const errorComponentGenerator = errors => errors
+    .map(soloError => <styles.ErrorComponent
+        key={actions.keySwitchCase(soloError)}
+        onClick={handleClickSpecError}>
+
+      <styles.ArrowDiv expanded={specErrorVisible} />
+      <styles.ErrorFile>{actions.typeSwitchCase(soloError)}</styles.ErrorFile>
+      <styles.ErrorType>{actions.overalltypeSwitchCase(soloError).name}:</styles.ErrorType>
+
+      <styles.ErrorShortMessage>
+        {actions.truncateMessage(actions.overalltypeSwitchCase(soloError).shortMessage)}
+      </styles.ErrorShortMessage>
+
+      {specErrorVisible
+        && <styles.ErrorFullMessage>
+            {actions.overalltypeSwitchCase(soloError).message}
+           </styles.ErrorFullMessage>}
+
+    </styles.ErrorComponent>);
+
   return (
-  <div>
-    {/* <ReactTable
-      data={props.errors}
-      pageSize={5}
-      columns={columns}
-    /> */}
-    <ErrorsBar>
-      <ErrorsHeader errors={errorsExist(props.errors)}>
-        {errorsLength(props.errors)} Errors
-      </ErrorsHeader>
-    </ErrorsBar>
-  </div>
+    <div>
+      {errorsVisible
+        && <styles.ErrorDisplay id="ErrorComponentDisplay">
+             {errorComponentGenerator(errors)}
+           </styles.ErrorDisplay>}
+
+      <styles.ErrorsHeader id="ErrorComponentHeader" {...headerProps} >
+        {actions.gtZero(errors.length)
+          && <styles.ErrorSymbol name="exclamation triangle" size="small" />}
+        {actions.errorArrayLength(errors)} Errors
+        <styles.ErrorBarArrow errorDisplay={errorsVisible} />
+      </styles.ErrorsHeader>
+    </div>
   );
 };
 
