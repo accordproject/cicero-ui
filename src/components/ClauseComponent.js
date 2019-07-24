9dadc0e245210f7aa2e11fe50f95410c639161b3
irmerk <jolenelanglinais@gmail.com>
2019-07-24 09:37:31 -0400

    refactor(components): abstract out styled components - I89
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/components/ClauseComponent.js
+++ src/components/ClauseComponent.js
@@ -4,63 +4,13 @@ import PropTypes from 'prop-types';
 import { Segment } from 'semantic-ui-react';
 
 /* Styling */
-import styled from 'styled-components';
-import './index.css';
+import * as styles from './styles';
 
 /* Icons */
 import * as deleteIcon from '../../assets/icons/trash';
 
 /* Actions */
 import titleGenerator from './actions';
-
-const ClauseWrapper = styled.div`
-  border: 1px solid ${props => props.clauseborder || '#19C6C7'};
-  border-radius: 3px;
-  background-color: ${props => props.clausebg || '#ECF0FA'};
-
-  display: grid;
-  grid-template-columns: 1fr 20px 20px 20px 20px 1px;
-  grid-template-rows: auto 1fr;
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-  grid-template-areas: "titleArea testIcon codeIcon editIcon deleteIcon endSpace"
-                       "bodyArea bodyArea bodyArea bodyArea bodyArea bodyArea";
-`;
-
-const ClauseHeader = styled.div`
-  margin: 12px 10px;
-
-  color: #939EBA;
-  font-family: ${props => props.headerfont || 'Graphik'};
-  font-size: 14px;
-  font-weight: 600;
-  line-height: 14px;
-
-  grid-area: titleArea;
-`;
-
-const ClauseBody = styled.div`
-  margin: 10px 10px 15px;
-
-  color: #141F3C;
-  font-family: ${props => props.bodyfont || 'Graphik'};
-  font-size: 15px;
-  line-height: 22px;
-
-  grid-area: bodyArea;
-`;
-
-const ClauseDelete = styled.svg`
-  fill: #939EBA;
-  cursor: pointer;
-
-  grid-area: deleteIcon;
-  place-self: center;
-
-  &:hover {
-    fill: ${props => props.clausedelete || '#19C6C7'};
-  }
-`;
 
 const deleteIconProps = {
   'aria-label': deleteIcon.type,
@@ -79,22 +29,23 @@ function ClauseComponent(props) {
   const errorsComponent = props.errors
     ? <Segment contentEditable={false} attached raised>{props.errors}</Segment>
     : null;
+
   return (
-    <ClauseWrapper
+    <styles.ClauseWrapper
       clauseborder={clauseProps.CLAUSE_BORDER}
       clausebg={clauseProps.CLAUSE_BACKGROUND}
     >
-      <ClauseHeader headerfont={clauseProps.HEADER_FONT} >
+      <styles.ClauseHeader headerfont={clauseProps.HEADER_FONT} >
         {titleGenerator(props.templateUri)} — SMART CLAUSE
-      </ClauseHeader>
-      <ClauseDelete {...deleteIconProps} clausedelete={clauseProps.CLAUSE_DELETE} >
+      </styles.ClauseHeader>
+      <styles.ClauseDelete {...deleteIconProps} clausedelete={clauseProps.CLAUSE_DELETE} >
         {deleteIcon.icon()}
-      </ ClauseDelete>
-      <ClauseBody bodyfont={clauseProps.BODY_FONT} >
+      </ styles.ClauseDelete>
+      <styles.ClauseBody bodyfont={clauseProps.BODY_FONT} >
         {props.children}
-      </ClauseBody>
+      </styles.ClauseBody>
     {errorsComponent}
-  </ClauseWrapper>
+  </styles.ClauseWrapper>
   );
 }
 
