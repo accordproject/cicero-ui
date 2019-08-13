97a0e840403d0849b5028ea3dc94a96f7ff983b6
irmerk <jolenelanglinais@gmail.com>
2019-08-13 16:49:08 -0400

    feat(Navigation): truncate headers with indents - I91
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/Navigation/Contract/index.js
+++ src/Navigation/Contract/index.js
@@ -1,28 +1,84 @@
 /* React */
 import React from 'react';
 import PropTypes from 'prop-types';
-
-/* Styling */
 import styled from 'styled-components';
 
-const NavWrapper = styled.div`
+/* Actions */
+import * as ACT from '../actions';
+
+/* Styling */
+// import * as SC from '../styles';
+
+export const ContractHeaders = styled.div`
     overflow-y: scroll !important;
+    overflow-x: hidden;
     padding-top: 10px;
 
     display: grid;
     grid-area: body;
 `;
 
+export const HeaderOne = styled.div`
+    margin: 2px 0;
+    height: 24px;
+    width: 185px;
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 16px;
+    letter-spacing: -0.5px;
+    line-height: 24px;
+`;
+
+export const HeaderTwo = styled.div`
+    margin: 2px 0 2px 20px;
+    height: 24px;
+    width: 185px;
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 16px;
+    letter-spacing: -0.5px;
+    line-height: 24px;
+`;
+
+export const HeaderThree = styled.div`
+    margin: 2px 0 2px 40px;
+    height: 24px;
+    width: 185px;
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 16px;
+    letter-spacing: -0.5px;
+    line-height: 24px;
+`;
+const isHeadingOne = input => input === 'heading_one';
+const isHeadingTwo = input => input === 'heading_two';
+const isHeadingThree = input => input === 'heading_three';
+
+const headerGenerator = headers => headers.map((header) => {
+  if (isHeadingOne(header.type)) {
+    return <HeaderOne key={header.key} >{ACT.truncateHeader(header)}</ HeaderOne>;
+  }
+  if (isHeadingTwo(header.type)) {
+    return <HeaderTwo key={header.key} >{ACT.truncateHeader(header)}</ HeaderTwo>;
+  }
+  if (isHeadingThree(header.type)) {
+    return <HeaderThree key={header.key} >{ACT.truncateHeader(header)}</ HeaderThree>;
+  }
+  return 'Error!';
+});
+
 /**
  * Represents all headers currently in a contract
  * @param {*} props
  */
-const ContractNavigation = props => (
-      <NavWrapper>
-          {props.headers
-            .map(header => <p style={{ color: 'white' }} key={header.key} >{header.text}</p>)}
-      </NavWrapper>
-);
+const ContractNavigation = (props) => {
+  const variable2 = 2;
+  return (
+      <ContractHeaders>
+          {headerGenerator(props.headers)}
+      </ContractHeaders>
+  );
+};
 
 ContractNavigation.propTypes = {
   headers: PropTypes.array
---
+++ src/Navigation/actions.js
@@ -0,0 +1,14 @@
+const isHeadingOne = input => input === 'heading_one';
+const isHeadingTwo = input => input === 'heading_two';
+const isHeadingThree = input => input === 'heading_three';
+
+const truncateHeading = (heading, length) => ((heading.length > length)
+  ? `${heading.substring(0, length)}...` : heading);
+
+
+export const truncateHeader = ({ type, text }) => {
+  if (isHeadingOne(type)) return truncateHeading(text, 22);
+  if (isHeadingTwo(type)) return truncateHeading(text, 18);
+  if (isHeadingThree(type)) return truncateHeading(text, 14);
+  return 'Error!';
+};
