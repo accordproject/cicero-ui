fc3525927f5ad2e6eb7618f7cdb65e039060fa8d
irmerk <jolenelanglinais@gmail.com>
2019-08-13 15:10:37 -0400

    feat(Navigation): implement contract navigation rendering from props - I91
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -3,42 +3,80 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 /* Styling */
-import styled from 'styled-components';
+import { NavigationWrapper } from './styles';
 
-const NavWrapper = styled.div`
-  padding-top: 10px;
-`;
+/* Constants */
+import { NAVIGATION, FILES } from './constants';
 
-// const LeftSidebar = styled.div`
-//   background-color: ${AP_THEME.DARK_BLUE};
-//   height: inherit;
-// `;
+/* Components */
+import ContractNavigation from './Contract';
+import FilesNavigation from './Files';
+import ComponentSwitch from './ComponentSwitch';
 
-// const LeftNavWrapper = styled.div`
-//     padding: 15px;
-//     overflow-x: hidden;
-//     background-color: ${AP_THEME.DARK_BLUE};
-//   `;
+/**
+ * Represents the overall navigation wrapper, consisting of
+ * two separate components, navigating between headers of the
+ * contract or files of a single clause
+ * @param {*} props
+ */
+const NavigationComponent = (props) => {
+  const navigationProps = props.navigationProps || Object.create(null);
 
-// const Heading = styled.h2`
-//   color: ${LEFT_NAV.HEADING};
-//   font-size: 14px;
-//   font-weight: 600;
-//   line-height: 14px;
-//   text-decoration: none;
-//   text-align: left;
-//   margin: 10px;
-// `;
+  const [navState, setNavState] = useState(NAVIGATION);
+  const navigationState = () => navState === NAVIGATION;
+  const filesState = () => navState === FILES;
 
+  const navigationWrapperProps = {
+    navMaxHeight: navigationProps.NAVIGATION_MAX_HEIGHT,
+    navWidth: navigationProps.NAVIGATION_WIDTH,
+  };
 
-const someSortOfComponent = props => (
-      <NavWrapper>
-          {props.headers.map(header => <p key={header.key} >{header.key}</p>)}
-      </NavWrapper>
-);
+  const switchProps = {
+    navState,
+    setNavState,
+  };
 
-someSortOfComponent.propTypes = {
-  headers: PropTypes.array
+  const navigationGenerator = (props) => {
+    if (navigationState()) return <ContractNavigation {...props} />;
+    if (filesState()) return <FilesNavigation {...props} />;
+    return 'Select Navigation or Files';
+  };
+
+  return (
+    <NavigationWrapper {...navigationWrapperProps} >
+        <ComponentSwitch {...switchProps} />
+        {navigationGenerator(props)}
+    </NavigationWrapper>
+  );
 };
 
-export default someSortOfComponent;
+NavigationComponent.propTypes = {
+  headers: PropTypes.array,
+  navigateHeader: PropTypes.func,
+  navigationProps: PropTypes.shape({
+    NAVIGATE_SWITCH_TITLE_ACTIVE_COLOR: PropTypes.string,
+    NAVIGATE_SWITCH_TITLE_INACTIVE_COLOR: PropTypes.string,
+    NAVIGATE_SWITCH_TITLE_FONT_FAMILY: PropTypes.string,
+
+    NAVIGATION_MAX_HEIGHT: PropTypes.string,
+    NAVIGATION_WIDTH: PropTypes.string,
+
+    CONTRACT_NAVIGATION_HEADER_COLOR: PropTypes.string,
+    CONTRACT_NAVIGATION_HEADER_HOVER_COLOR: PropTypes.string,
+    CONTRACT_NAVIGATION_CLAUSE_COLOR: PropTypes.string,
+    CONTRACT_NAVIGATION_CLAUSE_HOVER_COLOR: PropTypes.string,
+    CONTRACT_NAVIGATION_CLAUSE_HEADER_COLOR: PropTypes.string,
+    CONTRACT_NAVIGATION_CLAUSE_HEADER_HOVER_COLOR: PropTypes.string,
+
+    CLAUSE_NAVIGATION_TITLE_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_TITLE_HOVER_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_EXPANSION_ARROW_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_EXPANSION_ARROW_HOVER_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_FILE_DELETE_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_FILE_DELETE_HOVER_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_FILE_ADD_COLOR: PropTypes.string,
+    CLAUSE_NAVIGATION_FILE_ADD_HOVER_COLOR: PropTypes.string,
+  }),
+};
+
+export default NavigationComponent;
