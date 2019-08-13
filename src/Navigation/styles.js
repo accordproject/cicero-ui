26f969a3483af1ceaf96ef6263ff80c25ad2d342
irmerk <jolenelanglinais@gmail.com>
2019-08-13 16:48:33 -0400

    feat(styles): modularize styling - I91
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/Navigation/styles.js
+++ src/Navigation/styles.js
@@ -1,6 +1,8 @@
 /* Styling */
 import styled from 'styled-components';
 
+/* Overall Navigation Component */
+
 export const NavigationWrapper = styled.div`
     max-height: ${props => props.navMaxHeight || '80vh'};
     width: ${props => props.navWidth || '180px'};
@@ -10,7 +12,15 @@ export const NavigationWrapper = styled.div`
     grid-template-columns: 1fr 1fr;
     grid-template-rows: 20px 1fr;
     grid-template-areas: "navigation files" "body body";
+
+
+    ::-webkit-scrollbar-track {
+        width: 0px;
+        background: transparent;
+    }
 `;
+
+/* Navigation Component Switch */
 
 export const Title = styled.a`
     place-self: center;
@@ -38,4 +48,25 @@ export const Files = styled(Title)`
     color: ${props => (props.navState === 'FILES'
     ? (props.titleActive || '#19C6C7')
     : (props.titleInactive || '#86888D'))};
+`;
+
+/* Contract Navigation */
+
+export const ContractHeaders = styled.div`
+    overflow-y: scroll !important;
+    overflow-x: hidden;
+    padding-top: 10px;
+
+    display: grid;
+    grid-area: body;
+`;
+
+export const Header = styled.div`
+    height: 24px;
+    width: 185px;
+    color: #B9BCC4;
+    font-family: "IBM Plex Sans";
+    font-size: 16px;
+    letter-spacing: -0.5px;
+    line-height: 24px;
 `;
