909bfe1cd61f19f1441f0a9c9eaa3bdc145f147a
irmerk <jolenelanglinais@gmail.com>
2019-08-14 16:45:18 -0400

    feat(Navigations): handle truncation of clause - I91
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/Navigation/actions.js
+++ src/Navigation/actions.js
@@ -1,3 +1,4 @@
+const isClause = input => input === 'clause';
 const isHeadingOne = input => input === 'heading_one';
 const isHeadingTwo = input => input === 'heading_two';
 const isHeadingThree = input => input === 'heading_three';
@@ -7,6 +8,7 @@ const truncateHeading = (heading, length) => ((heading.length > length)
 
 
 export const truncateHeader = ({ type, text }) => {
+  if (isClause(type)) return truncateHeading(text, 22);
   if (isHeadingOne(type)) return truncateHeading(text, 22);
   if (isHeadingTwo(type)) return truncateHeading(text, 18);
   if (isHeadingThree(type)) return truncateHeading(text, 14);
