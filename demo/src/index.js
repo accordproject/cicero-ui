bc47ffa57b937166410301d0efc5d2b6d952a9fd
Dan Selman <danscode@selman.org>
2019-10-06 17:24:18 +0100

    (fix) update to latest markdown-editor
    Signed-off-by: Dan Selman <danscode@selman.org>

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,22 +1,16 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {
-  Header, Menu, Grid, Rail, Segment
-} from 'semantic-ui-react';
+import { Grid, Segment } from 'semantic-ui-react';
 
 // import { PluginManager } from '@accordproject/markdown-editor';
 import { SlateTransformer } from '@accordproject/markdown-slate';
 
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import ClauseEditor from '../../src/ClauseEditor';
 import ContractEditor from '../../src/ContractEditor';
 import ClausePlugin from '../../src/plugins/ClausePlugin';
-import VariablePlugin from '../../src/plugins/VariablePlugin';
 
 const slateTransformer = new SlateTransformer();
 const clausePlugin = ClausePlugin(null, null);
-const plugins = [VariablePlugin(), clausePlugin];
-// const pluginManager = new PluginManager(plugins);
 
 const templateUri = 'https://templates.accordproject.org/archives/acceptance-of-delivery@0.12.0.cta';
 const clauseText = `Acceptance of Delivery. "Party A" will be deemed to have completed its delivery obligations if in "Party B"'s opinion, the "Widgets" satisfies the Acceptance Criteria, and "Party B" notifies "Party A" in writing that it is accepting the "Widgets".
@@ -24,22 +18,6 @@ const clauseText = `Acceptance of Delivery. "Party A" will be deemed to have com
 Inspection and Notice. "Party B" will have 10 Business Days' to inspect and evaluate the "Widgets" on the delivery date before notifying "Party A" that it is either accepting or rejecting the "Widgets".
 
 Acceptance Criteria. The "Acceptance Criteria" are the specifications the "Widgets" must meet for the "Party A" to comply with its requirements and obligations under this agreement, detailed in "Attachment X", attached to this agreement.`;
-
-const getClauseMarkdown = async () => {
-  const rewriteClauseText = await clausePlugin.rewriteClause(templateUri, clauseText);
-
-  const acceptanceOfDeliveryClause = `\`\`\` <clause src="${templateUri}" clauseid="123">
-${rewriteClauseText}
-\`\`\`
-`;
-
-  const something = slateTransformer.fromMarkdown(acceptanceOfDeliveryClause);
-  console.log('something: ', something.toJSON());
-  // console.log('something: ', slateTransformer.toMarkdown(something))
-
-  // return fromMarkdown.convert(acceptanceOfDeliveryClause);
-  return slateTransformer.fromMarkdown(acceptanceOfDeliveryClause);
-};
 
 const getContractMarkdown = async () => {
   const rewriteClauseText = await clausePlugin.rewriteClause(templateUri, clauseText);
@@ -60,20 +38,9 @@ ${rewriteClauseText}
 };
 
 /**
- * A demo component that uses ContractEditor and
- * TemplateLoadingClauseEditor
+ * A demo component that uses ContractEditor
  */
 function Demo() {
-  /**
-   * Which demo is currently selected
-   */
-  const [activeItem, setActiveItem] = useState('clauseEditor');
-
-  /**
-   * Currently clause value
-   */
-  const [clauseValue, setClauseValue] = useState(null);
-
   /**
    * Currently contract value
    */
@@ -83,21 +50,7 @@ function Demo() {
    * Async rewrite of the markdown text to a slate value
    */
   useEffect(() => {
-    getClauseMarkdown().then(value => setClauseValue(value));
-  }, []);
-
-  /**
-   * Async rewrite of the markdown text to a slate value
-   */
-  useEffect(() => {
     getContractMarkdown().then(value => setContractValue(value));
-  }, []);
-
-  /**
-   * Called when the data in the clause editor has been modified
-   */
-  const onClauseChange = useCallback((value) => {
-    setClauseValue(value);
   }, []);
 
   /**
@@ -106,17 +59,6 @@ function Demo() {
   const onContractChange = useCallback((value) => {
     // console.log(JSON.stringify(value.toJSON(), null, 4));
     setContractValue(value);
-  }, []);
-
-  /**
-   * Called when the data in the clause editor has been parsed
-   */
-  const onParse = useCallback((newParseResult) => {
-    // console.log('onParse', newParseResult);
-  }, []);
-
-  const handleItemClick = useCallback((e, { name }) => {
-    setActiveItem(name);
   }, []);
 
   const editorProps = {
@@ -132,15 +74,7 @@ function Demo() {
     WIDTH: '600px',
   };
 
-  const demo = activeItem === 'clauseEditor'
-    ? <ClauseEditor
-        lockText={true}
-        value={clauseValue}
-        onChange={onClauseChange}
-        onParse={onParse}
-        editorProps={editorProps}
-      />
-    : <ContractEditor
+  const demo = <ContractEditor
         lockText={true}
         value={contractValue}
         onChange={onContractChange}
@@ -153,24 +87,6 @@ function Demo() {
         <Grid.Column>
           <Segment>
           {demo}
-          <Rail position='left'>
-            <Segment>
-              <Menu vertical>
-                <Menu.Item
-                  name='clauseEditor'
-                  active={activeItem === 'clauseEditor'}
-                  onClick={handleItemClick}
-                >
-                  <Header as='h4'>Clause Editor</Header>
-                  <p>Edit a single clause.</p>
-                </Menu.Item>
-                <Menu.Item name='contractEditor' active={activeItem === 'contractEditor'} onClick={handleItemClick}>
-                  <Header as='h4'>Contract Editor</Header>
-                  <p>Adds multiple clauses to a rich-text contract.</p>
-                </Menu.Item>
-              </Menu>
-            </Segment>
-          </Rail>
           </Segment>
         </Grid.Column>
       </Grid>
