d5400a9a41e7701a819afe540c4c246540425657
irmerk <jolenelanglinais@gmail.com>
2020-02-26 11:53:30 -0500

    chore(demo): revert demo - I233
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- demo/src/index.js
+++ demo/src/index.js
@@ -22,12 +22,12 @@ const templateUri2 = 'https://templates.accordproject.org/archives/acceptanceand
 const clauseText = `Late Delivery and Penalty.
 ----
 
-In case of delayed delivery<if id="forceMajeure" value="" whenTrue="%20except%20for%20Force%20Majeure%20cases%2C" whenFalse=""/>
+In case of delayed delivery<if id="forceMajeure" value="%20except%20for%20Force%20Majeure%20cases%2C" whenTrue="%20except%20for%20Force%20Majeure%20cases%2C" whenFalse=""/>
 <variable id="seller" value="%22Dan%22"/> (the Seller) shall pay to <variable id="buyer" value="%22Steve%22"/> (the Buyer) for every <variable id="penaltyDuration" value="2%20days"/>
-of <if id="forceMajeure" value="something something" whenTrue="%20except%20for%20Force%20Majeure%20cases%2C" whenFalse="something something"/> delay penalty amounting to <variable id="penaltyPercentage" value="10.5"/>% of the total value of the Equipment
+of delay penalty amounting to <variable id="penaltyPercentage" value="10.5"/>% of the total value of the Equipment
 whose delivery has been delayed. Any fractional part of a <variable id="fractionalPart" value="days"/> is to be
 considered a full <variable id="fractionalPart" value="days"/>. The total amount of penalty shall not however,
-exceed <variable id="capPercentage" value="55.0"/>% of the total value of the Equipment involved in late delivery. <if id="forceMajeure" value="something something" whenTrue="%20except%20for%20Force%20Majeure%20cases%2C" whenFalse="something something"/>
+exceed <variable id="capPercentage" value="55.0"/>% of the total value of the Equipment involved in late delivery.
 If the delay is more than <variable id="termination" value="15%20days"/>, the Buyer is entitled to terminate this <variable id="seller" value="%22Dan%22"/> Contract.`;
 
 const clauseText2 = `Acceptance of Delivery.
