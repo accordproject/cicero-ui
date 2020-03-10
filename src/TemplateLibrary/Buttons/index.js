dc2791fb0aa0aaecb80a345fbc3f1a8da6e00623
Sanyam Dogra <sdlord07@gmail.com>
2020-03-10 22:08:04 +0530

    button adhering to semantic UI
    Signed-off-by: Sanyam Dogra <sdlord07@gmail.com>
    Signed-off-by: elit-altum <manan.sharma311@gmail.com>

--- src/TemplateLibrary/Buttons/index.js
+++ src/TemplateLibrary/Buttons/index.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Button } from 'semantic-ui-react';
 
-const UploadButton = styled(button).attrs({
+const UploadButton = styled.button.attrs({
   'aria-label': 'Upload Button',
 })`
   position: relative;
