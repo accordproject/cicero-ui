316a4da297fbb98761f6168a171f696ab5df3a10
Sanyam Dogra <sdlord07@gmail.com>
2020-03-12 01:10:36 +0530

    feat(*): Added Cicero-UI links - I315
    Signed-off-by: Sanyam Dogra <sdlord07@gmail.com>
    Signed-off-by: elit-altum <manan.sharma311@gmail.com>

---
+++ .storybook/theme.js
@@ -0,0 +1,18 @@
+import { create } from '@storybook/theming';
+
+export default create ({
+    base: 'light',
+
+    colorPrimary: '#00C5CA',
+    colorSecondary: '#0066CC',
+
+    // UI
+    appBg: '#F3F3F5',
+    appContentBg: '#FFF',
+    appBorderColor: 'grey',
+    appBorderRadius: 4,
+
+    brandTitle: 'Cicero-UI',
+    brandUrl: 'https://github.com/accordproject/cicero-ui',
+    //brandImage: 'https://placehold.it/350x150',
+});
