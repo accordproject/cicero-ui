17f9e9424082a300155072da9c95978bd62f7bc2
jerrybuks <profkiti@gmail.com>
2020-03-12 21:46:28 +0100

    feat(components) adding bulleted list variables(changes) -I230
    Signed-off-by: jerrybuks <profkiti@gmail.com>

--- demo/src/index.js
+++ demo/src/index.js
@@ -19,6 +19,8 @@ const templateUri = 'https://templates.accordproject.org/archives/latedeliveryan
 
 const templateUri2 = 'https://templates.accordproject.org/archives/acceptance-of-delivery@0.13.1.cta';
 
+const templateUri3 = 'https://templates.accordproject.org/archives/volumediscountulist@0.2.1.cta'
+
 const clauseText = `Late Delivery and Penalty.
 ----
 
@@ -28,17 +30,7 @@ of delay penalty amounting to <variable id="penaltyPercentage" value="10.5"/>% o
 whose delivery has been delayed. Any fractional part of a <variable id="fractionalPart" value="days"/> is to be
 considered a full <variable id="fractionalPart" value="days"/>. The total amount of penalty shall not however,
 exceed <variable id="capPercentage" value="55.0"/>% of the total value of the Equipment involved in late delivery.
-If the delay is more than <variable id="termination" value="15%20days"/>, the Buyer is entitled to terminate this Contract.<if id="forceMajeure" value="%20except%20for%20Force%20Majeure%20cases%2C" whenTrue="%20except%20for%20Force%20Majeure%20cases%2C" whenFalse=""/>
--  <variable id="1" value="0.0"/>$ million <= Volume <  <variable id="millionTwo" value="1.0"/>$ million :  <variable id="percent" value="3.1"/>%
--  <variable id="1" value="0.0"/>$ million <= Volume <  <variable id="millionTwo" value="1.0"/>$ million :  <variable id="percent" value="3.1"/>%
--  <variable id="2" value="4.0"/>$ million <= Volume <  <variable id="millionTwo" value="2.0"/>$ million :  <variable id="percent" value="3.2"/>%
-
-last friday is today
--  <variable id="3" value="6.0"/>$ million <= Volume <  <variable id="millionTwo" value="3.0"/>$ million :  <variable id="percent" value="3.3"/>%
--  <variable id="4" value="7.0"/>$ million <= Volume <  <variable id="millionTwo" value="4.0"/>$ million :  <variable id="percent" value="3.4"/>%
-
-1.  Bird costs <variable id="4" value="7.0"/>$ 
-`;
+If the delay is more than <variable id="termination" value="15%20days"/>, the Buyer is entitled to terminate this Contract.`;
 
 const clauseText2 = `Acceptance of Delivery.
 ----
@@ -64,6 +56,48 @@ must meet for the <variable id="shipper" value="%22Party%20A%22"/> to comply wit
 obligations under this agreement, detailed in <variable id="attachment" value="%22Attachment%20X%22"/>, attached
 to this agreement.`;
 
+const clauseText3 = `Volume-Based Card Acceptance Agreement [Abbreviated]
+----
+
+This Agreement is by and between Card, Inc., a New York corporation, and you, the Merchant. By accepting the Card, you agree to be bound by the Agreement.
+Discount means an amount that we charge you for accepting the Card, which amount is:
+1. a percentage (Discount Rate) of the face amount of the Charge that you submit, or a flat per-
+   Transaction fee, or a combination of both; and/or
+1. a Monthly Flat Fee (if you meet our requirements).
+
+Transaction Processing and Payments. Our Card acceptance, processing, and payment requirements are set forth in the Merchant Regulations. Some requirements are summarized here for ease of reference, but do not supersede the provisions in the Merchant Regulations.
+Payment for Charges. We will pay you, through our agent, according to your payment plan in US dollars for the face amount of Charges submitted from your Establishments less all applicable deductions, rejections, and withholdings, which include:
+1. the Discount,
+1. any amounts you owe us or our Affiliates,
+1. any amounts for which we have Chargebacks and
+1. any Credits you submit.
+
+Your initial Discount is indicated in the Agreement or otherwise provided to you in writing by us. In addition to your Discount we may charge you additional fees and assessments, as listed in the Merchant Regulations or as otherwise provided to you in writing by us. We may adjust any of these amounts and may change any other amount we charge you for accepting the Card.
+
+### SETTLEMENT
+
+#### Settlement Amount.
+
+Our agent will pay you according to your payment plan, as described below, in US dollars for the face amount of Charges submitted from your Establishments less all applicable deductions, rejections, and withholdings, which include:
+1. the Discount,
+1. any amounts you owe us or our Affiliates,
+1. any amounts for which we have Chargebacks, and
+1. any Credits you submit.
+
+Our agent will subtract the full amount of all applicable deductions, rejections, and withholdings, from this payment to you (or debit your Bank Account), but if it cannot, then you must pay it promptly upon demand.
+
+#### Discount.
+
+The Discount is determined according to the following table:
+
+- <variable id="volumeAbove" value="0.0"/>$ million <= Volume < <variable id="volumeUpTo" value="1.0"/>$ million : <variable id="rate" value="3.1"/>%
+- <variable id="volumeAbove" value="1.0"/>$ million <= Volume < <variable id="volumeUpTo" value="10.0"/>$ million : <variable id="rate" value="3.1"/>%
+- <variable id="volumeAbove" value="10.0"/>$ million <= Volume < <variable id="volumeUpTo" value="50.0"/>$ million : <variable id="rate" value="2.9"/>%
+- <variable id="volumeAbove" value="50.0"/>$ million <= Volume < <variable id="volumeUpTo" value="500.0"/>$ million : <variable id="rate" value="2.5"/>%
+- <variable id="volumeAbove" value="500.0"/>$ million <= Volume < <variable id="volumeUpTo" value="1000.0"/>$ million : <variable id="rate" value="1.2"/>%
+- <variable id="volumeAbove" value="1000.0"/>$ million <= Volume < <variable id="volumeUpTo" value="1000000.0"/>$ million : <variable id="rate" value="0.1"/>%
+`
+
 const getContractSlateVal = async () => {
   const lateDeliveryandPenaltyClause = `\`\`\` <clause src="${templateUri}" clauseid="123">
 ${clauseText}
@@ -75,6 +109,11 @@ ${clauseText2}
 \`\`\`
 `;
 
+const volumeDiscountList = `\`\`\` <clause src="${templateUri3}" clauseid="123">
+${clauseText3}
+\`\`\`
+`;
+
   const defaultContractMarkdown = `# Heading One
   This is text. This is *italic* text. This is **bold** text. This is a [link](https://clause.io). This is \`inline code\`.
   
@@ -83,7 +122,10 @@ ${clauseText2}
   Paragraph in between
   
   ${acceptanceOfDeliveryClause}
+
+  second Paragraph in between
   
+  ${volumeDiscountList}
   Fin.
   `;
   return Value.fromJSON(slateTransformer.fromMarkdown(defaultContractMarkdown));
