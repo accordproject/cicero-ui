d0f36ae6d98e64dd8af4bbdbaa02bb13702c5403
irmerk <jolenelanglinais@gmail.com>
2020-03-31 15:12:20 -0400

    refactor(src): pause at work in progress
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

---
+++ src/plugins/withClauseSchema.js
@@ -0,0 +1,17 @@
+const CLAUSE = 'clause';
+const VARIABLE = 'variable';
+const CONDITIONAL = 'conditional';
+
+const INLINES = { [VARIABLE]: true };
+const VOIDS = {};
+
+/* eslint no-param-reassign: 0 */
+const withClauseSchema = (editor) => {
+  const { isVoid, isInline } = editor;
+  editor.isVoid = element => (VOIDS[element.type] || isVoid(element));
+  editor.isInline = element => (INLINES[element.type] || isInline(element));
+
+  return editor;
+};
+
+export default withClauseSchema;
