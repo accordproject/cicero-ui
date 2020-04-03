a7a16b9faa8679dbf5a7a5fe06ddc8327162e7e4
Diana Lease <dianarlease@gmail.com>
2020-04-03 18:58:44 -0400

    feat(withVariables): add logic for editing beginning and end of variables
    Signed-off-by: Diana Lease <dianarlease@gmail.com>
    Signed-off-by: irmerk <jolenelanglinais@gmail.com>

--- src/plugins/withVariables.js
+++ src/plugins/withVariables.js
@@ -2,60 +2,25 @@ import isHotkey from 'is-hotkey';
 import { Transforms, Node, Editor } from 'slate';
 import inVariable from '../utilities/inVariable';
 
-import isToolbarMethodHelper from '../utilities/isToolbarMethod';
-
-// export const isEditable = ((editor, code) => {
-//   const { selection } = editor;
-//   console.log('EDITOR - ', editor);
-//   const inVariable = inVariableHelper(editor);
-
-//   console.log(`${code} - in variable ${inVariable}`);
-
-//   if (inVariable && isToolbarMethodHelper(code)) {
-//     return false;
-//   }
-
-//   if (code === 'backspace') {
-//     if (inVariable) {
-//       // if we hit backspace and are at the zeroth
-//       // position of a variable prevent deleting the char
-//       // that precedes the variable
-//       return anchor.offset > 0;
-//     }
-
-//     // if we hit backspace and are outside of a variable
-//     // allow deleting the last char of the variable
-//     // IFF the variable has more than 1 char
-//     // AND IFF the selection does not include anything outside the variable
-//     const prev = value.document.getPreviousSibling(anchor.path);
-//     return prev && anchor.offset === 0
-//         && prev.type === 'variable'
-//         && prev.getFirstText().text.length > 1
-//         && value.selection.start.key === value.selection.end.key;
-//   }
-
-//   if (!inVariable && code === 'input') {
-//     // if are outside of a variable allowing
-//     // extending the variable
-//     const prev = value.document.getPreviousSibling(anchor.path);
-
-//     const extendingVar = prev && anchor.offset === 0 && prev.type === 'variable';
-//     if (extendingVar) {
-//       editor.moveToEndOfNode(prev);
-//     }
-//     return extendingVar;
-//   }
-
-//   // disallow enter within variables!
-//   return code !== 'enter' && inVariable;
-// });
-
 /* eslint no-param-reassign: 0 */
 const withVariables = (editor) => {
   const { insertText, isInline } = editor;
   editor.insertText = (text) => {
-    // console.log('in custom insert text', text);
-    insertText(text);
+    const nextNode = Editor.next(editor, { at: editor.selection.focus.path });
+    const textLength = Node.get(editor, editor.selection.focus.path).text.length;
+
+    // if the current focus is at the end of a node and the next node is a variable
+    // move focus to the start of the variable node
+    if (nextNode && nextNode[0].type === 'variable' && textLength === editor.selection.focus.offset) {
+      Transforms.select(editor, nextNode[1]);
+      Transforms.collapse(editor, { edge: 'start' });
+    }
+
+    if (Node.parent(editor, editor.selection.focus.path).type === 'variable') {
+      Transforms.insertText(editor, text);
+    } else {
+      insertText(text);
+    }
   };
 
   editor.isInline = element => (element.type === 'variable' ? true : isInline(element));
@@ -63,9 +28,9 @@ const withVariables = (editor) => {
 };
 
 export const isEditable = (lockText, editor, event) => {
-  console.log('isInsideClause --- ', editor.isInsideClause());
-  if (!lockText || !editor.isInsideClause()) return true;
-  console.log('isInVariable -- ', inVariable(editor));
+  if (!lockText || !editor.isInsideClause() || isHotkey('mod+c', event) || isHotkey('mod+x', event)) {
+    return true;
+  }
   const { selection } = editor;
   const textLength = Node.get(editor, editor.selection.focus.path).text.length;
   if (inVariable(editor)) {
@@ -74,23 +39,16 @@ export const isEditable = (lockText, editor, event) => {
       if (textLength === 1) {
         return false;
       }
-
-      // if we hit backspace and are at the zeroth
-      // position of a variable prevent deleting the char
-      // that precedes the variable
+      // if we hit backspace and are at the zeroth position of a
+      // variable prevent deleting the char that precedes the variable
       return selection.anchor.offset > 0;
     }
-
-    // Do not allow user to type at end of variable
-    // because that change will happen outside variable
-    // if (textLength === selection.focus.offset) return false;
   }
-
-  // if (inVariable(editor) && selection.anchor.offset === 0) {
-  //   // if at the 0th index of the variable, move to start of variable
-  //   // otherwise our change will be applied outside of the variable
-  //   Transforms.move(editor, { edge: 'start' });
-  // }
+  const nextNode = Editor.next(editor, { at: editor.selection.focus.path });
+  // if the current focus is at the end of a node & the next node is a variable allow editing
+  if (nextNode && nextNode[0].type === 'variable' && textLength === editor.selection.focus.offset) {
+    return true;
+  }
   return inVariable(editor);
 };
 
--- src/utilities/inVariable.js
+++ src/utilities/inVariable.js
@@ -3,12 +3,7 @@ import { isEqual } from 'lodash';
 
 const inVariable = (editor) => {
   const { selection } = editor;
-
   if (!selection) return false;
-  // console.log('node --- ', Node.get(editor, editor.selection.focus.path));
-
-  // console.log('parent --- ', Node.parent(editor, editor.selection.focus.path));
-
   // check if the user has selected more than just a variable
   if (!isEqual(selection.anchor.path, selection.focus.path)) return false;
   return Node.parent(editor, editor.selection.focus.path).type === 'variable';
