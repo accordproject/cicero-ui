f86cf6290029e2bdabc5f19060a0a898f8c9df72
lenvi <lenvin@oykuapp.com>
2020-04-14 00:46:53 +0530

    feat(storybook): Add webpack config - i315
    Signed-off-by: lenvi <lenvin@oykuapp.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,36 @@
-module.exports = {
-    stories: ["../src/**/*.stories.[tj]s"],
-    addons: [
-      // '@storybook/addon-actions',
-      '@storybook/addon-knobs/register',
-      '@storybook/addon-notes/register',
-      '@storybook/addon-a11y/register',
-      // '@storybook/addon-storyshots'
-    ]
-  };
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = module.exports = {
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
+    });
+    config.plugins.push(new MiniCssExtractPlugin({ filename: "[name].css" }));
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: "demo/src/index.html",
+        mountId: "demo",
+        title: "YES"
+      })
+    );
+    config.plugins.push(new CleanWebpackPlugin());
+    config.plugins.push(new webpack.IgnorePlugin(/jsdom$/));
+    config = { ...config, node: { fs: "empty", net: "empty", tls: "empty" } };
+    // Return the altered config
+    return config;
+  },
+  stories: ["../src/**/*.stories.[tj]s"],
+  addons: [
+    // '@storybook/addon-actions',
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-notes/register",
+    "@storybook/addon-a11y/register"
+    // '@storybook/addon-storyshots'
+  ]
+};
