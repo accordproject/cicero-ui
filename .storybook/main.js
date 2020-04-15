44d128d05f5d6d7e552e283da934b0da34b84994
lenvi <lenvin@oykuapp.com>
2020-04-15 17:49:13 +0530

    fix(storybook): Fix webpack_config that caused issue with production build - i315
    Signed-off-by: lenvi <lenvin@oykuapp.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = module.exports = {
   webpackFinal: async (config, { configType }) => {
@@ -11,15 +8,6 @@ module.exports = module.exports = {
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../")
     });
-    config.plugins.push(new MiniCssExtractPlugin({ filename: "[name].css" }));
-    config.plugins.push(
-      new HtmlWebpackPlugin({
-        template: "demo/src/index.html",
-        mountId: "demo",
-        title: "Cicero UI Components Demo"
-      })
-    );
-    config.plugins.push(new CleanWebpackPlugin());
     config.plugins.push(new webpack.IgnorePlugin(/jsdom$/));
     config = { ...config, node: { fs: "empty", net: "empty", tls: "empty" } };
     // Return the altered config
